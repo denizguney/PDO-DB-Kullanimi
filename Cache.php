60411184c89d536e6b8ff09db69c0db04669ec9d
denizguney <31777283+denizguney@users.noreply.github.com>
2017-11-04 02:45:27 +0200

    Add files via upload

---
+++ Cache.php
@@ -0,0 +1,69 @@
+<?php
+/*
+*
+* @ Deniz Güney Yıldırım
+* @ dnzgny99@gmail.com
+* @ 0534 665 56 29
+*/
+
+
+namespace Buki;
+
+class Cache
+{
+  private $cacheDir = null;
+  private $cache    = null;
+  private $finish   = null;
+
+  function __construct($dir = null, $time = 0)
+  {
+    if(!file_exists($dir))
+            mkdir($dir, 0755);
+
+    $this->cacheDir = $dir;
+    $this->cache = $time;
+    $this->finish = time() + $time;
+  }
+
+  public function setCache($sql, $result)
+  {
+    if (is_null($this->cache))
+      return false;
+
+    $cacheFile = $this->cacheDir . $this->fileName($sql) . '.cache';
+    $cacheFile = fopen($cacheFile, 'w');
+
+    if($cacheFile)
+      fputs($cacheFile, json_encode(["data" => $result, "finish" => $this->finish]));
+
+    return;
+  }
+
+  public function getCache($sql, $array = false)
+  {
+    if (is_null($this->cache))
+      return false;
+
+    $cacheFile = $this->cacheDir . $this->fileName($sql) . ".cache";
+
+    if (file_exists($cacheFile))
+    {
+      $cache = json_decode(file_get_contents($cacheFile), $array);
+
+      if (($array ? $cache["finish"] : $cache->finish) < time())
+      {
+        unlink($cacheFile);
+        return;
+      }
+      else
+        return ($array ? $cache["data"] : $cache->data);
+    }
+
+    return false;
+  }
+
+  private function fileName($name)
+  {
+    return md5($name);
+  }
+}
